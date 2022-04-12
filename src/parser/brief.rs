f3595bd9210a759d82e31665b1be893b4092b819
numToStr <vikasraj1911@gmail.com>
2022-04-12 15:56:09 +0530

    feat: generate help for `---@brief`

--- src/parser/brief.rs
+++ src/parser/brief.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use chumsky::{prelude::just, select, Parser};
 
 use crate::{impl_parse, TagType};
@@ -11,9 +13,15 @@ pub struct Brief {
 impl_parse!(Brief, {
     select! {
         TagType::Comment(x) => x,
-        TagType::Empty => '\n'.into()
+        TagType::Empty => "".into()
     }
     .repeated()
     .delimited_by(just(TagType::BriefStart), just(TagType::BriefEnd))
     .map(|desc| Self { desc })
 });
+
+impl Display for Brief {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.desc.join("\n"))
+    }
+}
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -62,6 +62,7 @@ impl_parse!(Node, {
 impl Display for Node {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self {
+            Self::Brief(x) => x.fmt(f),
             Self::Func(x) => x.fmt(f),
             Self::Class(x) => x.fmt(f),
             _ => todo!(),
