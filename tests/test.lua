2ad346a52dc14daddd12c84592919c77410b57de
Vikas Raj <24727447+numToStr@users.noreply.github.com>
2022-07-05 18:43:41 +0530

    feat: multiline `---@param` description (#22)

--- tests/test.lua
+++ tests/test.lua
@@ -159,4 +159,30 @@ end
 ---| 'v'
 ---| 'V' # Visual Line Mode
 
+---Trigger a rebuild of one or more projects.
+---@param opts table|nil optional configuration options:
+---  * {select_mode} (JdtProjectSelectMode) Show prompt
+---     to select projects or select all. Defaults
+---     to 'prompt'
+---
+---  * {full_build} (boolean) full rebuild or
+---     incremental build. Defaults to true (full build)
+---@param reserverd table|nil reserved for the future use
+---@return boolean
+function U.multi_line(opts, reserverd)
+    print(vim.inspect(opts), vim.inspect(reserverd))
+
+    return true
+end
+
+---Multiline but missing description
+---@param n number
+---This is a special
+---number
+---@param m number And this is also
+---@return number
+function U.missing_desc(n, m)
+    return n + m
+end
+
 return U
