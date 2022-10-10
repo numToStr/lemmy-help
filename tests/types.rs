868d262ddfb4d3171d4cca28d41292b389da1286
numToStr <vikasraj1911@gmail.com>
2022-10-10 18:26:15 +0530

    more test

--- tests/types.rs
+++ tests/types.rs
@@ -26,35 +26,61 @@ fn types() {
         };
     }
 
-    //         "table<string, fun(a: string): string>",
-    //         "table<fun(), table<string, number>>",
-    //         "table<string, fun(a: string, b: table<string, boolean>)>",
-    //         "{ get: string, set: string }",
-    //         "{ get: fun(a: unknown): unknown, set: fun(a: unknown) }",
-    //         "table<string, string|table<string, string>>",
-    //         "table<string, string>[]",
-    //         "string",
-    //         "any[]",
-    //         "any|any|any",
-    //         "any|string|number",
-    //         "any|string|number|fun(a: string)|table<string, number>|userdata[]",
-    //         "fun(a: string, c: string, d: number): table<string, number[]>[]",
-    //         "fun(a: string, c: string[], d: number[][]): table<string, number>[]",
+    check!("nil", Ty::Nil);
+    check!("any", Ty::Any);
+    check!("unknown", Ty::Unknown);
+    check!("boolean", Ty::Boolean);
+    check!("string", Ty::String);
+    check!("number", Ty::Number);
+    check!("integer", Ty::Integer);
+    check!("function", Ty::Function);
+    check!("thread", Ty::Thread);
+    check!("userdata", Ty::Userdata);
+    check!("lightuserdata", Ty::Lightuserdata);
+    check!("Any-Thing.El_se", Ty::Ref("Any-Thing.El_se".into()));
 
     check!(
-        "string[]|string",
-        Ty::Union(b!(Ty::Array(b!(Ty::String))), b!(Ty::String))
+        "(string|number|table<string, string[]>)[]",
+        Ty::Array(b!(Ty::Union(
+            b!(Ty::String),
+            b!(Ty::Union(
+                b!(Ty::Number),
+                b!(Ty::Table(Some((
+                    b!(Ty::String),
+                    b!(Ty::Array(b!(Ty::String)))
+                ))))
+            ))
+        )))
     );
 
     check!(
-        r#"'"g@"'|string[]|'"g@$"'|number"#,
-        Ty::Union(
-            b!(Ty::Ref(r#""g@""#.into())),
-            b!(Ty::Union(
-                b!(Ty::Array(b!(Ty::String))),
-                b!(Ty::Union(b!(Ty::Ref(r#""g@$""#.into())), b!(Ty::Number)))
+        "table<string, { get: string, set: string }>[]",
+        Ty::Array(b!(Ty::Table(Some((
+            b!(Ty::String),
+            b!(Ty::Dict(vec![
+                TypeVal::Req("get".into(), Ty::String),
+                TypeVal::Req("set".into(), Ty::String),
+            ]))
+        )))))
+    );
+
+    check!(
+        "table<string, fun(a: string): string>",
+        Ty::Table(Some((
+            b!(Ty::String),
+            b!(Ty::Fun(
+                vec![TypeVal::Req("a".into(), Ty::String)],
+                Some(vec![Ty::String])
             ))
-        )
+        )))
+    );
+
+    check!(
+        "table<fun(), table<string, number>>",
+        Ty::Table(Some((
+            b!(Ty::Fun(vec![], None)),
+            b!(Ty::Table(Some((b!(Ty::String), b!(Ty::Number)))))
+        )))
     );
 
     check!(
@@ -92,10 +118,7 @@ fn types() {
         )
     );
 
-    //         "table<string, { get: string, set: string }>[]",
-    //         "(string|number|table<string, string[]>)[]",
-    //         "table<string, string|string[]|boolean>[]",
-    //         "fun(a: string, b: (string|table<string, number>)[]|boolean, c: string[], d: number[][]): string|string[]",
+    // "fun(a: string, b: (string|table<string, number>)[]|boolean, c: string[], d: number[][]): string|string[]",
 
     check!(
         "fun(
@@ -144,6 +167,64 @@ fn types() {
                 )
             ],
             Some(vec![Ty::Table(Some((b!(Ty::String), b!(Ty::String))))])
+        )
+    );
+
+    check!(
+        "{
+            inner: string,
+            get: fun(a: unknown),
+            set: fun(a: unknown),
+            __proto__?: { _?: unknown }
+        }",
+        Ty::Dict(vec![
+            TypeVal::Req("inner".into(), Ty::String),
+            TypeVal::Req(
+                "get".into(),
+                Ty::Fun(vec![TypeVal::Req("a".into(), Ty::Unknown)], None,)
+            ),
+            TypeVal::Req(
+                "set".into(),
+                Ty::Fun(vec![TypeVal::Req("a".into(), Ty::Unknown)], None)
+            ),
+            TypeVal::Opt(
+                "__proto__".into(),
+                Ty::Dict(vec![TypeVal::Opt("_".into(), Ty::Unknown)])
+            )
+        ])
+    );
+
+    check!(
+        r#"'"g@"'|string[]|'"g@$"'|number"#,
+        Ty::Union(
+            b!(Ty::Ref(r#""g@""#.into())),
+            b!(Ty::Union(
+                b!(Ty::Array(b!(Ty::String))),
+                b!(Ty::Union(b!(Ty::Ref(r#""g@$""#.into())), b!(Ty::Number)))
+            ))
+        )
+    );
+
+    check!(
+        "any|any|string|(string|number)[]|fun(a: string)|table<string, number>|userdata[]",
+        Ty::Union(
+            b!(Ty::Any),
+            b!(Ty::Union(
+                b!(Ty::Any),
+                b!(Ty::Union(
+                    b!(Ty::String),
+                    b!(Ty::Union(
+                        b!(Ty::Array(b!(Ty::Union(b!(Ty::String), b!(Ty::Number))))),
+                        b!(Ty::Union(
+                            b!(Ty::Fun(vec![TypeVal::Req("a".into(), Ty::String)], None)),
+                            b!(Ty::Union(
+                                b!(Ty::Table(Some((b!(Ty::String), b!(Ty::Number))))),
+                                b!(Ty::Array(b!(Ty::Userdata)))
+                            ))
+                        ))
+                    ))
+                ))
+            ))
         )
     );
 }
